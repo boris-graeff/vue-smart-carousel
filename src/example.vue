dcd3b265a8cfdad6392d86a16b9d549daeecd81f
bof <boris.graeff@gmail.com>
2019-04-11 10:55:34 +0200

    Add scss support and create dev env with example file

---
+++ src/example.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+    <section class="example-1">
+      <vue-carousel-component>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div><strong>4</strong></div>
+      </vue-carousel-component>
+    </section>
+
+    <section class="example-2">
+      <vue-carousel-component>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div><strong>4</strong></div>
+      </vue-carousel-component>
+    </section>
+  </div>
+</template>
+
+<script>
+  import VueCarouselComponent from './vue-carousel-component'
+
+  export default {
+    components: {
+        VueCarouselComponent
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .app {
+    section {
+      border: 1px solid tomato;
+    }
+  }
+
+  .example-2 {
+    max-width: 1000px;
+    margin: auto;
+  }
+</style>
